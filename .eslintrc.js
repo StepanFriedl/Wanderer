2588a0cb861cc1919d00b075bf52051fec0964b2
Viktor Rádi <rviktor87@web-fejlesztes.eu>
2020-08-12 15:54:39 +0200

    disable forcing eol formats

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   rules: {
     'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.ts'] }],
     '@typescript-eslint/indent': [2, 2],
+    'linebreak-style': 0,
     'import/extensions': [
       'error',
       'ignorePackages',
